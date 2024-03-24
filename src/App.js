981c281a94395af1abc4defe51a2ddadc9a5c1b3
Антон Кучер <kapetrovi4@gmail.com>
2024-03-24 19:11:40 +0300

    fixing some style problems

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Portfolio from './components/Portfolio';
 import Player from './components/Player';
 import { useEffect } from 'react';
 import { images } from './constants';
-import Hammer from 'hammerjs';
 
 function App() {
 	const isMobile = window.innerWidth <= 1115;
@@ -13,21 +12,7 @@ function App() {
 		images.forEach((src) => {
 			const img = new Image();
 			img.src = src;
-			console.log(img);
 		});
-	}, []);
-
-	useEffect(() => {
-		const hammer = new Hammer(document.body);
-		hammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
-
-		hammer.on('swipedown', () => {
-			window.location.reload();
-		});
-
-		return () => {
-			hammer.destroy();
-		};
 	}, []);
 
 	return (
